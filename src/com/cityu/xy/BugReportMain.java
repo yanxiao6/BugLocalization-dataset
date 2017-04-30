5ac98e43fa0b37c3a0167f5012fde7c2b24c0137
yanxiao6 <hhu_xiaoyan@163.com>
2017-04-30 22:13:53 +0800

    clearer name

--- src/com/cityu/xy/BugReportMain.java
+++ src/com/cityu/xy/BugReportMain.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BugReportAstMain {
+public class BugReportMain {
 	private final static String TAG = BugReportAstMain.class.getSimpleName();
 
 	public static void main(String[] args) throws Exception {
